557808d237b23b658f6233dddd80e1b393eb65b6
lavrov-alexey <lavrov-box@mail.ru>
2023-02-03 21:55:10 +0300

    Lesson-3. News from JSON and get-params in URL

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,10 @@
 # from django.shortcuts import render
 
-# from django.http import HttpResponse
 # from django.views.generic import View
-from datetime import datetime
+import json
 
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 
@@ -17,12 +18,29 @@ class NewsPageView(TemplateView):
     def get_context_data(self, **kwargs):
         # Get all previsious data
         context = super().get_context_data(**kwargs)
-        # Create your own data
-        context["news_title"] = "ГРОМКИЙ НОВОСТНОЙ ЗАГОЛОВОК"
-        context["news_preview"] = "Предварительное описание новости, которое заинтересует каждого"
-        context["range"] = range(1, 6)
-        context["datetime_obj"] = datetime.now()
+
+        # вариант задания тестовой новости "вручную"
+        # context["news_title"] = "ГРОМКИЙ НОВОСТНОЙ ЗАГОЛОВОК"
+        # context["news_preview"] = "Предварительное описание новости, которое заинтересует каждого"
+        # context["datetime_obj"] = datetime.now()
+        # context["range"] = range(1, 6)
+
+        # открываем файл с новостями на чтение (встроенными ср-ми Питона) и сохраняем в контекст список из новостей
+        with open(settings.BASE_DIR / "news.json") as news_file:
+            # новости из списка будем перебирать в цикле в шаблоне news по полям
+            context["news_list"] = json.load(news_file)
+
         return context
+
+    # переопределим у контроллера метод get
+    def get(self, *args, **kwargs):
+        # если был передан get-параметр c ключом 'q' в словаре с get-параметрами поля запроса
+        # - сохраняем его, если нет - None
+        query = self.request.GET.get("q", None)
+        if query:  # если запрос по ключу q был (не None), то делаем редирект на поисковую систему с фразой для поиска
+            return HttpResponseRedirect(f"https://google.ru/search?q={query}")
+        # если нет - продолжаем норм. работу контроллера
+        return super().get(*args, **kwargs)
 
 
 class NewsWithPaginatorView(NewsPageView):
