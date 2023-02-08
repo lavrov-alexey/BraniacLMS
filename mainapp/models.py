3a1cc8605fcc0303567baa974bd9823699dabab1
lavrov-alexey <lavrov-box@mail.ru>
2023-02-08 20:07:55 +0300

    Lesson-4. Fixtures

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,38 +1,72 @@
-# Create your models here.
 from django.db import models
 
-# в целях оптимизации - создадим "константный" словарь для передачи в полях параметров возможности пустых
-NULLABLE = {"blank": True, "null": True}
 
-
-# Базовая модель с общими полями и методами для всех моделей
-class BaseModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Изменено")
-    deleted = models.BooleanField(default=False, verbose_name="Удален")
-
-    # переопределяем удаление на "мягкое" удаление - в виде изменения статуса, без реального удаления данных
-    def __del__(self):
-        self.deleted = True
-        self.save()
-
-    class Meta:
-        abstract = True  # чтобы в БД не создавались лишние взаимосвязи базы с ост. таблицами (моделями)
-        # с "-" обратная сортировка (как DESC), без него - прямая (как ASC). Список/кортеж
-        ordering = [
-            "-created_at",
-        ]
-
-
-class News(BaseModel):
+class News(models.Model):
     title = models.CharField(max_length=256, verbose_name="Title")
     preambule = models.CharField(max_length=1024, verbose_name="Preambule")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
     body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return f"{self.pk} {self.title}"
 
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class Courses(models.Model):
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    num = models.PositiveIntegerField(verbose_name="Lesson number")
+    title = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.course.name} | {self.num} | {self.title}"
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
     class Meta:
-        verbose_name = "новость"
-        verbose_name_plural = "новости"
+        ordering = ("course", "num")
+
+
+class CourseTeachers(models.Model):
+    course = models.ManyToManyField(Courses)
+    name_first = models.CharField(max_length=128, verbose_name="Name")
+    name_second = models.CharField(max_length=128, verbose_name="Surname")
+    day_birth = models.DateField(verbose_name="Birth date")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
